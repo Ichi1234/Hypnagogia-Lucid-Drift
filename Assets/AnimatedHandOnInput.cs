b934a97ac30a78a5ff2a8b9982a0c525cc824084
Kasidet Uthaiwiwatkul <ichigamer111th@gmail.com>
2026-05-26 11:12:03 +0700

    feat: try animated hand

---
+++ Assets/AnimatedHandOnInput.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class AnimatedHandOnInput : MonoBehaviour
+{
+    public InputActionProperty triggerValue;
+    public InputActionProperty gripValue;
+
+    public Animator handAnimator;
+
+    private void Update()
+    {
+        float trigger = triggerValue.action.ReadValue<float>();
+        float grip = gripValue.action.ReadValue<float>();
+
+        handAnimator.SetFloat("Trigger", trigger);
+        handAnimator.SetFloat("Grip", grip);
+
+    }
+}
